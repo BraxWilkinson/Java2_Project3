1e4f66bc96d4b233c6c9bb286af8016bff3dc699
Valdus145 <55852104+Valdus145@users.noreply.github.com>
2019-10-26 13:41:40 -0500

    Half of DateTimeOne is printing

--- DateTimeOne.java
+++ DateTimeOne.java
@@ -1,6 +1,7 @@
 import java.time.LocalDateTime;
 import java.time.ZoneId;
 import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.HashMap;
 
@@ -8,8 +9,6 @@ public class DateTimeOne extends MesoDateTimeOneAbstract {
 	
 	private LocalDateTime myTime;
 	private HashMap<String,String> stringHash;
-	private DateTimeFormatter printPattern = DateTimeFormatter.ofPattern("hh:mm");
-	private DateTimeFormatter hashPattern = DateTimeFormatter.ofPattern("MM/dd/yyyy hh:mm");
 	
 	public DateTimeOne() {
 		myTime = LocalDateTime.now();
@@ -30,13 +29,15 @@ public class DateTimeOne extends MesoDateTimeOneAbstract {
    }
    
    public void dateTimeNow() {
-	   System.out.println(printPattern.format(myTime));
+	   DateTimeFormatter nowPattern = DateTimeFormatter.ofPattern("MM/dd/yyyy hh:mm a");
+	   System.out.println("Current Date/Time: " + nowPattern.format(myTime));
    }
    
    public void dateTimeOfOtherCity() {
-	   stringHash.put("GMT", LocalDateTime.now(ZoneId.of("GMT")).format(hashPattern));
-	   stringHash.put("BST", LocalDateTime.now(ZoneId.of("BST")).format(hashPattern));
-	   stringHash.put("CST", LocalDateTime.now(ZoneId.of("CST")).format(hashPattern));
+	   DateTimeFormatter hashPattern = DateTimeFormatter.ofPattern("MM/dd/yyyy hh:mm");
+	   stringHash.put("GMT", LocalDateTime.now(ZoneId.of("Z")).format(hashPattern));
+	   stringHash.put("BST", LocalDateTime.now(ZoneId.of("Asia/Dhaka")).format(hashPattern));
+	   stringHash.put("CST", LocalDateTime.now(ZoneId.of("America/Chicago")).format(hashPattern));
 	   
 	   System.out.println("Time on Server: " + myTime.format(hashPattern));
 	   System.out.println("GMT: " + stringHash.get("GMT"));
@@ -45,15 +46,16 @@ public class DateTimeOne extends MesoDateTimeOneAbstract {
    }
    
    public void dateTimeDifferentZone() {
-	   stringHash.put("ZST", "11/05/2018 19:59");
-	   stringHash.put("AST", "10/01/2020 19:59");
-	   
-	   System.out.println("GMT: " + LocalDateTime.now(ZoneId.of("GMT")).format(printPattern));
-	   System.out.println("BST: " + LocalDateTime.now(ZoneId.of("BST")).format(printPattern));
-	   System.out.println("CST: " + LocalDateTime.now(ZoneId.of("CST")).format(printPattern));
+	   DateTimeFormatter printPattern = DateTimeFormatter.ofPattern("hh:mm");
+	   System.out.println("GMT: " + LocalDateTime.now(ZoneId.of("Z")).format(printPattern));
+	   System.out.println("BST: " + LocalDateTime.now(ZoneId.of("Asia/Dhaka")).format(printPattern));
+	   System.out.println("CST: " + LocalDateTime.now(ZoneId.of("America/Chicago")).format(printPattern));
    }
    
    public void timeZoneHashMap() {
+	   stringHash.put("ZST", "11/05/2018 19:59");
+	   stringHash.put("AST", "10/01/2020 19:59");
+	   
 	   HashMap<String,String> secondStringHash = new HashMap<String,String>();
 	   secondStringHash.put(stringHash.get("GMT"),"GMT");
 	   secondStringHash.put(stringHash.get("BST"),"BST");
@@ -62,14 +64,31 @@ public class DateTimeOne extends MesoDateTimeOneAbstract {
 	   secondStringHash.put(stringHash.get("AST"),"AST");
 	   
 	   DateTimeFormatter arrayPattern = DateTimeFormatter.ofPattern("yyyy-MM-ddThh:mm");
-	   LocalDateTime[] dateArray = new LocalDateTime[5];
-	   dateArray[0] = LocalDateTime.parse(stringHash.get("GMT"),arrayPattern);
-	   dateArray[1] = LocalDateTime.parse(stringHash.get("BST"),arrayPattern);
-	   dateArray[2] = LocalDateTime.parse(stringHash.get("CST"),arrayPattern);
-	   dateArray[3] = LocalDateTime.parse(stringHash.get("ZST"),arrayPattern);
-	   dateArray[4] = LocalDateTime.parse(stringHash.get("AST"),arrayPattern);
+	   ArrayList<LocalDateTime> dateArray = new ArrayList<LocalDateTime>();
+	   dateArray.add(LocalDateTime.parse(stringHash.get("GMT"),arrayPattern));
+	   dateArray.add(LocalDateTime.parse(stringHash.get("BST"),arrayPattern));
+	   dateArray.add(LocalDateTime.parse(stringHash.get("CST"),arrayPattern));
+	   dateArray.add(LocalDateTime.parse(stringHash.get("ZST"),arrayPattern));
+	   dateArray.add(LocalDateTime.parse(stringHash.get("AST"),arrayPattern));
 	   
 	   System.out.println("Print Style 1: ");
+	   ArrayList<String> sortedKeys1 = new ArrayList<String>(stringHash.keySet()); 
+	   Collections.sort(sortedKeys1);  
+	   for (String x : sortedKeys1) {
+		   System.out.println(x + stringHash.get(x));
+	   }
 	   
+	   System.out.println("Print Style 3: ");
+	   ArrayList<String> sortedKeys2 = new ArrayList<String>(secondStringHash.keySet()); 
+	   Collections.sort(sortedKeys2);  
+	   for (String y : sortedKeys2) {
+		   System.out.println(y);
+	   }
+	   
+	   System.out.println("Print Style 3: ");
+	   Collections.sort(dateArray);  
+	   for (LocalDateTime z : dateArray) {
+		   System.out.println(z);
+	   }
    }
 }
